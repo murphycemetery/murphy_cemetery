d71f7f26bfd0529bfe2a2292dcb2f38088c89582
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2021-04-09 18:17:18 -0500

    admin home page and burial search

---
+++ adminIndex.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Murphy Cemetery - Burial Search</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <a href="index.php"><li>Home</li></a>
+          <a href="about.php"><li>About/History</li></a>
+          <a href="location.php"><li>Location</li></a>
+          <a href="burialSearch.php"><li>Burial Search</li></a>
+          <a href="contact.php"><li>Contact</li></a>
+          <a href="logout.php"><li>Logout</li></a>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <h1>Administrator View</h1>
+      <a href="addBurial.php"><button class="add">Add Burial</button></a>
+      <a href="burialSearchIndividual.php"><div class="burial">
+        <img src="" alt="" />
+        <h2 class="fullName">John Lee Doe</h2>
+        <p class="yearsLived">December 7, 1936 - May 4, 2020</p>
+        <p class="plot">Plot: 27</p>
+        <p class="grave">Grave: 3</p>
+        <a href="adminEdit.php"><button class="edit">Edit</button></a>
+        <a href="adminDelete.php"><button class="delete">Delete</button></a>
+      </div></a>
+
+      <a href="burialSearchIndividual.php"><div class="burial">
+        <img src="" alt="" />
+        <h2 class="fullName">John Lee Doe</h2>
+        <p class="yearsLived">December 7, 1936 - May 4, 2020</p>
+        <p class="plot">Plot: 27</p>
+        <p class="grave">Grave: 3</p>
+        <a href="adminEdit.php"><button class="edit">Edit</button></a>
+        <a href="adminDelete.php"><button class="delete">Delete</button></a>
+      </div></a>
+
+      <a href="burialSearchIndividual.php"><div class="burial">
+        <img src="" alt="" />
+        <h2 class="fullName">John Lee Doe</h2>
+        <p class="yearsLived">December 7, 1936 - May 4, 2020</p>
+        <p class="plot">Plot: 27</p>
+        <p class="grave">Grave: 3</p>
+        <a href="adminEdit.php"><button class="edit">Edit</button></a>
+        <a href="adminDelete.php"><button class="delete">Delete</button></a>
+      </div></a>
+    </main>
+  </body>
+</html>
